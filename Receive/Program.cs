eb7760a7ffc487f5ac8e93f16593462523809777
Jason Linden <jasonlinden01@gmail.com>
2018-05-27 20:10:04 +0200

    Add project files.

---
+++ Receive/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ReceiveMQ
+{
+    // This is the consumer
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, e) =>
+                    {
+                        var body = e.Body;
+                        var mesage = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", mesage);
+                    };
+
+                    channel.BasicConsume(
+                        queue: "hello",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace SendMQ
+{
+    class Program
+    {
+        // This is the publisher
+        // The publisher will connect to RabbitMQ, send a single message, then exit.
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // To send, we must declare a queue for us to send to; then we can publish a message to the queue:
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body
+                    );
+
+                    System.Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                System.Console.WriteLine(" Press [enter] to exit.");
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
